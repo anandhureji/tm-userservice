8de36a352b96a7818c7cf594b8781ec91de9a7e5
anandhureji <anandhureji33@gmail.com>
2025-03-23 14:04:58 +0530

    initial commit

---
+++ src/main/java/com/taskmanagment/userservice/model/config/ApplicationConfiguration.java
@@ -0,0 +1,51 @@
+package com.taskmanagment.userservice.model.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class ApplicationConfiguration {
+
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.sessionManagement(
+                management->management.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS
+                )
+        ).authorizeHttpRequests(
+                Authorize->Authorize.requestMatchers("/api/**")
+                        .authenticated()
+                        .anyRequest().permitAll()
+        ).addFilterBefore(null, BasicAuthenticationFilter.class)
+                .csrf(csrf->csrf.disable())
+                .cors(cors->cors.configurationSource(corsConfigurationSource()))
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Collections.singletonList("*"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
+                return cfg;
+            }
+        };
+    }
+}
